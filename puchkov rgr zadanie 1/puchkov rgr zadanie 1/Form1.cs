f6cd32b1a252216146449eb1fbd475e4c7116881
zubarvika <zubarvikaaa@mail.ru>
2022-03-15 23:56:01 +0300

    Lab2

---
+++ puchkov rgr zadanie 1/puchkov rgr zadanie 1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puchkov_rgr_zadanie_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, n;
+            n = 1;
+            int M = Convert.ToInt32(textBox1.Text);
+            double x1 = Convert.ToDouble(textBox2.Text);
+            double x2 = Convert.ToDouble(textBox3.Text);
+            double A = Convert.ToDouble(textBox4.Text);
+            double[] x = new double[25];
+            double[] y = new double[25];
+            x[1] = x1;
+            x[M + 2] = x2;
+
+            for (i = 2; i <= (M + 1); i++)
+            {
+                x[i] = x1 + (x2 - x1) * n / (M + 1);
+                n++;
+            }
+
+            for (i = 1; i <= (M + 2); i++)
+            {
+                if (x[i] >= 0)
+                {
+                    if ((x[i] == 0) && (A == 0))
+                    {
+                        listBox1.Items.Add(i + ".");
+                        listBox1.Items.Add("Для x [ " + i + " ] = " + x[i] + " решений нет");
+                        listBox1.Items.Add("");
+                    }
+
+                    else
+                    {
+                        y[i] = -(Math.Sqrt(Math.Sqrt(16*Math.Pow(A,4)+4*Math.Pow(A,2)*Math.Pow(A+x[i],2))-Math.Pow(A,2)));
+                        listBox1.Items.Add(i + ".");
+                        listBox1.Items.Add("Для x [ " + i + " ] = " + x[i]);
+                        listBox1.Items.Add("y [" + i + "] = " + y[i]);
+                        listBox1.Items.Add("");
+                    }
+                }
+
+                else
+                {
+                    if ((A == 0) && (x[i]<0))
+                    {
+                        listBox1.Items.Add(i + ".");
+                        listBox1.Items.Add("Для x [ " + i + " ] = " + x[i] + " решений нет");
+                        listBox1.Items.Add("");
+                    }
+
+                    else
+                    {
+                        y[i] = Math.Sqrt(Math.Pow(A, 2)-Math.Pow(A + x[i], 2))-2*A;
+                        listBox1.Items.Add(i + ".");
+                        listBox1.Items.Add("Для x [ " + i + " ] = " + x[i]);
+                        listBox1.Items.Add("y [" + i + "] = " + y[i]);
+                        listBox1.Items.Add("");
+                    }
+                }
+
+                button1.Enabled = false;
+            }
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            listBox1.Items.Clear();
+            button1.Enabled = true;
+            textBox1.Focus();
+        }
+    }
+}
+
+
---
+++ puchkov rgr zadanie 1/puchkov rgr zadanie 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("puchkov rgr zadanie 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("puchkov rgr zadanie 1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d1848b05-eb4e-43a1-8c5b-09d1aaeb8dde")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
